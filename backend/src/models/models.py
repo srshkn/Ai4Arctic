b643d1d223bc9aed78a4464fb385fdd98d677f5d
srshkn <snuqlstyas@gmail.com>
2026-04-23 01:42:21 +0500

    updating models 1.0

--- backend/src/models/models.py
+++ backend/src/models/models.py
@@ -1,5 +1,20 @@
+import uuid
+
+from sqlalchemy import String
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column
+
 from src.db import Base
 
 
+# Таблица пользователей
 class User(Base):
     __tablename__ = "users"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )
+    name: Mapped[str] = mapped_column(
+        String(75), nullable=False, unique=True, index=True
+    )
+    password_hash: Mapped[str] = mapped_column(String(255))
