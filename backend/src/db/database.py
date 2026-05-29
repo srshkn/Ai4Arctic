7a08bf36dc298e1e2e7e5bb47cd4403978302097
srshkn <snuqlstyas@gmail.com>
2026-05-30 03:44:12 +0500

    updating tests & DB urls

--- backend/src/db/database.py
+++ backend/src/db/database.py
@@ -11,7 +11,7 @@ from src.core import get_settings
 
 def build_engine(db_url: str | None = None):
     if db_url is None:
-        db_url = get_settings().DB_URL
+        db_url = str(get_settings().ASYNC_DB_URL)
     return create_async_engine(db_url, echo=True)
 
 
